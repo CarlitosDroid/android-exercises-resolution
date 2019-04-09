d745cacf5e939d92d3285d54e533dfad1647cfb6
Carlos Leonardo Camilo Vargas Huamán <carlosleonardovargashuaman@gmail.com>
2019-04-09 17:13:27 -0500

    feature(shake_action): Add ShakeDetector

--- app/src/main/java/com/example/android_exercises_resolution/MainActivity.kt
+++ app/src/main/java/com/example/android_exercises_resolution/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.example.android_exercises_resolution
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private val sensorManager by lazy {
+        getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+
+    private val accelerometerSensor by lazy {
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+    }
+
+    private val shakeDetector by lazy {
+        ShakeDetectorListener {
+            Toast.makeText(this@MainActivity, getString(R.string.shaked), Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onResume() {
+        super.onResume()
+        // Add the following line to register the Session Manager Listener onResume
+        sensorManager.registerListener(shakeDetector, accelerometerSensor, SensorManager.SENSOR_DELAY_UI)
+    }
+
+    override fun onPause() {
+        // Add the following line to unregister the Sensor Manager onPause
+        sensorManager.unregisterListener(shakeDetector)
+        super.onPause()
+    }
+
 }
